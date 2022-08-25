5042ce48251db7afb9c1f46185fb348fa3ee188f
khallekan <okeolalekanisaac@gmail.com>
2022-08-25 15:21:11 +0100

    [feat] update user avatar

--- src/components/Settings/Avatar.tsx
+++ src/components/Settings/Avatar.tsx
@@ -1,13 +1,82 @@
-import { useState } from "react";
-import { Box, Text, Avatar, Button, Center } from "@chakra-ui/react";
+import { useCallback, useEffect, useState } from "react";
+import { Box, Text, Avatar, Button, Center, useToast } from "@chakra-ui/react";
+import { useUser } from "@utils/hooks/useUser";
+import { avatarSets } from "@utils/prodData";
+import { userAPI } from "@utils/api";
 
 export default function AvatarView() {
   const [selectedAvatar, setSelectedAvatar] = useState("");
+  const [allAvatars, setAllAvatars] = useState<string[]>([]);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const toast = useToast();
 
-  const generateRandomAvatar = () => {
-    const rand = Math.floor(1000 + Math.random() * 9000);
-    return rand;
+  const { data: user } = useUser();
+
+  const generateRandomAvatar = (length: number) => {
+    // generate random alphanumeric value
+    return Math.random().toString(16).substring(2, length);
   };
+
+  // runs once on every page load
+  useEffect(() => {
+    const avatarUrl = avatarSets.map((item) => {
+      const ran = generateRandomAvatar(8);
+      return `https://robohash.org/${ran}?set=${item.id}&size=200x200`;
+    });
+
+    setAllAvatars(avatarUrl);
+  }, []);
+
+  const onSubmit = useCallback(
+    async (data: string) => {
+      setIsSubmitting(true);
+      try {
+        const result = await userAPI.updateUser({ avatar: data });
+        if (result) {
+          toast({
+            position: "top-right",
+            duration: 2000,
+            isClosable: true,
+            render: () => (
+              <Box
+                color="white"
+                p={3}
+                bg="black"
+                borderRadius={10}
+                textAlign="center"
+                fontSize="xs"
+              >
+                {result?.data?.message}
+              </Box>
+            ),
+          });
+        }
+      } catch (error: any) {
+        console.log(error);
+
+        toast({
+          position: "top-right",
+          duration: 9000,
+          isClosable: true,
+          render: () => (
+            <Box
+              color="white"
+              p={3}
+              bg="#fa4e37"
+              borderRadius={10}
+              textAlign="center"
+              fontSize="xs"
+            >
+              {error ?? "Error, try again"}
+            </Box>
+          ),
+        });
+      } finally {
+        setIsSubmitting(false);
+      }
+    },
+    [toast]
+  );
 
   return (
     <Box pt="1rem" color="#000">
@@ -20,11 +89,28 @@ export default function AvatarView() {
         gap="20px"
         justifyContent="center"
       >
-        <Avatar src={selectedAvatar} name="anonry" size="xl" />
-
-        <Button variant="primary" px="10px" py="5px">
-          Save
-        </Button>
+        {/* if selected avatar is an empty string display the current user avatar if not show the selected user avatar*/}
+        <Avatar
+          src={
+            selectedAvatar.length ? selectedAvatar : user?.data?.data?.avatar
+          }
+          name="anonry"
+          size="xl"
+        />
+        {/* if there is a selected avatar */}
+        {!!selectedAvatar.length && (
+          <Button
+            variant="primary"
+            px="10px"
+            py="5px"
+            _focus={{ outline: "none" }}
+            type="submit"
+            isLoading={isSubmitting}
+            onClick={() => onSubmit(selectedAvatar)}
+          >
+            Save
+          </Button>
+        )}
       </Box>
 
       <Box
@@ -38,18 +124,16 @@ export default function AvatarView() {
         gap="1.8rem"
       >
         {allAvatars.map((item, index) => {
-          const ran = generateRandomAvatar();
-          const nsrc = `https://robohash.org/${ran}?set=${item.id}&size=500x500`;
           return (
             <Center key={index}>
               <Avatar
                 size="lg"
-                src={nsrc}
+                src={item}
                 mx="auto"
                 cursor="pointer"
                 transition="all 0.5s"
                 _hover={{ transform: "scale(1.1)" }}
-                onClick={(e) => console.log(nsrc)}
+                onClick={(e) => setSelectedAvatar(item)}
               />
             </Center>
           );
@@ -58,44 +142,3 @@ export default function AvatarView() {
     </Box>
   );
 }
-
-const allAvatars = [
-  {
-    id: "set2",
-  },
-  {
-    id: "set2",
-  },
-  {
-    id: "set2",
-  },
-  {
-    id: "set2",
-  },
-
-  {
-    id: "set3",
-  },
-  {
-    id: "set3",
-  },
-  {
-    id: "set3",
-  },
-  {
-    id: "set3",
-  },
-
-  {
-    id: "set4",
-  },
-  {
-    id: "set4",
-  },
-  {
-    id: "set4",
-  },
-  {
-    id: "set4",
-  },
-];
