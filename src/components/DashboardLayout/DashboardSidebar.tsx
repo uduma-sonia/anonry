0caf0970b97390abd9ab84f0427d95719098e278
uduma sonia <sohnyauduma@gmail.com>
2022-10-06 18:11:50 +0100

    auth page transition

--- src/components/DashboardLayout/DashboardSidebar.tsx
+++ src/components/DashboardLayout/DashboardSidebar.tsx
@@ -71,7 +71,8 @@ export default function DashboardSidebar() {
 
       <UnorderedList listStyleType="none" spacing={0}>
         {defaultRoutes.map(({ route, label, icon, sub_route }) => {
-          const isActive = router.asPath.includes(route);
+          const isActive =
+            router.asPath.includes(route) || router.asPath.includes(sub_route);
 
           return (
             <ListItem
