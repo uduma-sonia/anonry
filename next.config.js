fa915dd6d4c13fcd1d092686ca5a561d87a61d89
uduma sonia <sohnyauduma@gmail.com>
2022-10-20 02:33:13 +0100

    added PWA

--- next.config.js
+++ next.config.js
@@ -1,13 +1,44 @@
+// const withPWA = require("next-pwa");
+
+// /** @type {import('next').NextConfig} */
+
+// module.exports = withPWA({
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//   },
+//   i18n: {
+//     locales: ["en"],
+//     defaultLocale: "en",
+//   },
+//   reactStrictMode: true,
+// images: {
+//   domains: ["robohash.org"],
+// },
+// });
+
+const withPWA = require("next-pwa");
+
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
-  reactStrictMode: true,
   images: {
     domains: ["robohash.org"],
   },
+  pwa: {
+    dest: "public",
+    register: false,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
 };
 
-module.exports = nextConfig;
+module.exports =
+  process.env.NODE_ENV === "development" ? nextConfig : withPWA(nextConfig);
