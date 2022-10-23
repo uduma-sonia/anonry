ce1153fadeb9fffd334430dac80f770cc674f310
uduma sonia <sohnyauduma@gmail.com>
2022-10-23 02:36:20 +0100

    [fix] next auth error

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-/* eslint-disable import/no-anonymous-default-export */
+// /* eslint-disable import/no-anonymous-default-export */
 import { api, authAPI } from "@utils/api";
 import Credentials from "next-auth/providers/credentials";
 import { NextApiRequest, NextApiResponse } from "next";
@@ -20,7 +20,134 @@ const refreshAccessToken = async (token: any) => {
   }
 };
 
-const options = {
+// const options = {
+//   providers: [
+//     Credentials({
+//       id: "credentials",
+//       name: "Credentials",
+//       credentials: {
+//         identifier: { label: "identifier", type: "string" },
+//         password: { label: "password", type: "password" },
+//       },
+//       async authorize(credentials, req) {
+//         try {
+//           const result = await authAPI.login({
+//             identifier: credentials?.identifier!,
+//             password: credentials?.password!,
+//           });
+//           return result?.data;
+//         } catch (error: any) {
+//           return null;
+//         }
+//       },
+//     }),
+
+//     Credentials({
+//       id: "google",
+//       name: "google",
+//       credentials: {
+//         token: { label: "identifier", type: "string" },
+//       },
+//       async authorize(credentials, req) {
+//         try {
+//           const result = await authAPI.googleLogin({
+//             token: credentials?.token!,
+//           });
+//           return result?.data;
+//         } catch (error: any) {
+//           if (error) {
+//             throw new Error(error ?? "Something went wrong");
+//           }
+//           return null;
+//         }
+//       },
+//     }),
+
+//     Credentials({
+//       id: "refresh",
+//       name: "refresh",
+//       credentials: {
+//         refreshToken: { label: "refreshToken", type: "string" },
+//       },
+//       async authorize(credentials, req) {
+//         try {
+//           const result = await authAPI.refreshToken({
+//             refreshToken: credentials?.refreshToken!,
+//           });
+//           return result?.data;
+//         } catch (error: any) {
+//           if (error) {
+//             throw new Error(error ?? "Something went wrong");
+//           }
+//           return null;
+//         }
+//       },
+//     }),
+
+//     Credentials({
+//       id: "verify-email",
+//       name: "VerifyEmail",
+//       credentials: {
+//         otp: { label: "OTP", type: "string" },
+//         email: { label: "email", type: "email" },
+//       },
+//       async authorize(credentials, req) {
+//         try {
+//           const result: any = await authAPI.verifyEmail({
+//             otp: credentials?.otp!,
+//             email: credentials?.email!,
+//           });
+//           return result.data;
+//         } catch (error: any) {
+//           if (error) {
+//             throw new Error(error ?? "Something went wrong");
+//           }
+//           return null;
+//         }
+//       },
+//     }),
+//   ],
+//   session: {
+//     jwt: true,
+//   },
+//   callbacks: {
+//     async session({ session, token }: any) {
+//       //   Returns result from jwt token below
+
+//       const signInData = token?.signInData as any;
+//       session.user = signInData?.data?.user;
+//       session.token = signInData?.data?.access_token;
+//       session.refresh = signInData?.data?.refresh_token;
+//       session.token_expires = signInData?.data?.access_token_expires;
+//       session.refresh_expires = signInData?.data?.refresh_token_expires;
+//       return session;
+//     },
+//     async jwt({ token, user }: any) {
+//       if (user) {
+//         //   Sets user to result from authorize
+//         token.signInData = user;
+//       }
+//       const currentDate = Date.now();
+
+//       if (currentDate >= token?.signInData?.data?.access_token_expires) {
+//         token.signInData.data = await refreshAccessToken(token.signInData.data);
+//       }
+//       return token;
+//     },
+//   },
+// secret: "testing",
+//   pages: {
+//     signIn: "/api/auth/signin",
+//   },
+// };
+
+// export default (
+//   req: NextAuthOptions | NextApiRequest,
+//   res: NextApiResponse<any>
+//   //@ts-ignore
+// ) => NextAuth(req, res, options);
+
+export default NextAuth({
   providers: [
     Credentials({
       id: "credentials",
@@ -41,7 +168,6 @@ const options = {
         }
       },
     }),
-
     Credentials({
       id: "google",
       name: "google",
@@ -83,7 +209,6 @@ const options = {
         }
       },
     }),
-
     Credentials({
       id: "verify-email",
       name: "VerifyEmail",
@@ -108,8 +233,10 @@ const options = {
     }),
   ],
   session: {
-    jwt: true,
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 330 days
   },
+  secret: "testing",
   callbacks: {
     async session({ session, token }: any) {
       //   Returns result from jwt token below
@@ -135,14 +262,4 @@ const options = {
       return token;
     },
   },
-  secret: "testing",
-  pages: {
-    signIn: "/api/auth/signin",
-  },
-};
-
-export default (
-  req: NextAuthOptions | NextApiRequest,
-  res: NextApiResponse<any>
-  //@ts-ignore
-) => NextAuth(req, res, options);
+});
