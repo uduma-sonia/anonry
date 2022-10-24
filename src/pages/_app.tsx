3c56db77a303a8af972d8b5f6c99facb49bf4ac0
uduma sonia <sohnyauduma@gmail.com>
2022-10-24 15:00:01 +0100

    [fix] next-auth error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import theme from "@lib/theme";
 import { SessionProvider } from "next-auth/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { SWRConfig } from "swr";
+import theme from "@lib/theme";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // @ts-ignore
+  const nextSession = pageProps.session;
+
   return (
     <GoogleOAuthProvider clientId="641963392237-lp0j6msphamku5srl8kjdligdcd146d7.apps.googleusercontent.com">
-      <SessionProvider session={pageProps?.session}>
+      <SessionProvider session={nextSession}>
         <SWRConfig
           value={{
             fallback: pageProps.fallback ?? {},
